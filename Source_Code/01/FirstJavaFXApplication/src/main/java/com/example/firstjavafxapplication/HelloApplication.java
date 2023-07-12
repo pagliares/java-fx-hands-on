239db7a2211242ddfd15fafd6a5d94d96c88b59c
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-07-12 10:58:28 -0300

    Add screenshots of files of first example

--- Source_Code/01/FirstJavaFXApplication/src/main/java/com/example/firstjavafxapplication/HelloApplication.java
+++ Source_Code/01/FirstJavaFXApplication/src/main/java/com/example/firstjavafxapplication/HelloApplication.java
@@ -1,10 +1,8 @@
 package com.example.firstjavafxapplication;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
